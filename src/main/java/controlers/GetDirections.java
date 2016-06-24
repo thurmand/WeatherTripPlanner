9b115b6a72beec39d83016f923a78ddf52c14929
elfrevaldes <elfre.valdes@gmail.com>
2016-06-24 17:30:46 -0600

    Get dirrection not working yet, and result.jsp with some css

---
+++ src/main/java/controlers/GetDirections.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlers;
+
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsResult;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Elfre
+ */
+@WebServlet(urlPatterns = {"/GetDirections"})
+public class GetDirections extends HttpServlet {
+    
+    private GeoApiContext context;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String origin = request.getParameter("origin");
+        String destination = request.getParameter("destination");
+        
+        // creating the request for the address
+        this.context = context
+            .setQueryRateLimit(3)
+            .setConnectTimeout(1, TimeUnit.SECONDS)
+            .setReadTimeout(1, TimeUnit.SECONDS)
+            .setWriteTimeout(1, TimeUnit.SECONDS);
+        
+        
+        int x = 3;
+        // syncronize call 
+        DirectionsResult result;
+        try {
+            result = DirectionsApi.getDirections(context, origin, destination).await();
+            request.setAttribute("result", result);
+        } catch (Exception ex) {
+            Logger.getLogger(GetDirections.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        request.getRequestDispatcher("result.jsp").forward(request, response);
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/result.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : result
+    Created on : Jun 24, 2016, 5:13:30 PM
+    Author     : Elfre
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Results</title>
+    </head>
+    <body>
+        <h1>This are your direction:</h1>
+        <p>${result}</p>
+    </body>
+</html>
